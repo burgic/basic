2bff96996cb664b1abb45454f5780eb5d1425456
Christian Burgin <ceburgin@gmail.com>
2024-11-26 12:43:09 +0000

    debugging, client details

--- src/components/Adviser/ClientDetails.tsx
+++ src/components/Adviser/ClientDetails.tsx
@@ -90,12 +90,46 @@ const ClientDetails: React.FC = () => {
       <ul>
         {incomes.map((income) => (
           <li key={income.id}>
-            {income.type}: {income.amount} ({income.frequency})
+            {income.type}: ${income.amount} ({income.frequency})
           </li>
         ))}
       </ul>
 
-      {/* Similarly for Expenditures, Assets, Liabilities, and Goals */}
+      <h3>Expenditures</h3>
+      <ul>
+        {expenditures.map((expenditure) => (
+          <li key={expenditure.id}>
+            {expenditure.category}: £{expenditure.amount} ({expenditure.frequency})
+          </li>
+        ))}
+      </ul>
+
+      <h3>Assets</h3>
+      <ul>
+        {assets.map((asset) => (
+          <li key={asset.id}>
+            {asset.type}: {asset.description} valued at £{asset.value}
+          </li>
+        ))}
+      </ul>
+
+      <h3>Liabilities</h3>
+      <ul>
+        {liabilities.map((liability) => (
+          <li key={liability.id}>
+            {liability.type}: {liability.description} amounting to £{liability.amount} at {liability.interest_rate}% interest
+          </li>
+        ))}
+      </ul>
+
+      <h3>Goals</h3>
+      <ul>
+        {goals.map((goal) => (
+          <li key={goal.id}>
+            {goal.goal}: Target of £{goal.target_amount} in {goal.time_horizon} years
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
